acb4f9a33fc28403cde4b67797dfb478bb30c4a9
urydev <laugomezramos@gmail.com>
2018-02-10 20:43:40 +0100

    ejercicios dia 8

---
+++ Laura/ejercicios_clase3.js
@@ -0,0 +1,16 @@
+// ejercicio 1
+
+	Proceso contadorPositivo
+		
+		controlador = 1
+		
+		Escribir "Empieza la cuenta!"
+		
+		Mientras controlador <= 100 Hacer
+			Escribir controlador
+			controlador = controlador +1
+		Fin Mientras
+		
+		Escribir "Terminó la cuenta mágica..."
+		
+	FinProceso
